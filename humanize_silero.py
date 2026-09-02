8fb08f63b9e940fe9c3632e0473715b453ee1b1e
Иван Войтков (Ivan Voitkov) <ivan_voitkov@proton.me>
2026-09-02 18:17:01 +0500

    Update humanize_silero.py

--- humanize_silero.py
+++ humanize_silero.py
@@ -1,3 +1,23 @@
+"""
+ArgonNeuroReader
+Copyright (c) 2026 Ivan Voitkov
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Author: Ivan Voitkov
+Website: https://argon-studio.ru/
+GitHub: https://github.com/Argonstudio/ArgonNeuroReader
+"""
+
 import cupy as cp
 from cupyx.scipy import signal as cp_signal
 import numpy as np
